f42ad395f584e2033a02dcecf51470779b7cecda
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-05-10 17:56:15 +0900

    fix: 창업보육센터 메뉴가 제대로 저장되지 않는 문제 수정

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import asyncio
 from datetime import datetime, timedelta
 
 import requests
+from requests.exceptions import ChunkedEncodingError
 from sqlalchemy import select
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import sessionmaker
@@ -37,12 +38,27 @@ async def execute_script(session):
     for restaurant_id, in session.execute(restaurant_query):
         for day_delta in range(-5, 5):
             day = now + timedelta(days=day_delta)
-            urls.append((restaurant_id, f"https://www.hanyang.ac.kr/web/www/re{restaurant_id}", day))
-    responses = [(restaurant_id, requests.get(
-        f"{url}?p_p_id=foodView_WAR_foodportlet&_foodView_WAR_foodportlet_sFoodDateYear={day.year}"
-        f"&_foodView_WAR_foodportlet_sFoodDateMonth={day.month - 1}&_foodView_WAR_foodportlet_sFoodDateDay={day.day}",
-    ), day) for restaurant_id, url, day in urls]
-    job_list = [get_menu_data(session, restaurant_id, response, day) for restaurant_id, response, day in responses]
+            urls.append((
+                restaurant_id,
+                f"https://www.hanyang.ac.kr/web/www/re{restaurant_id}",
+                day,
+            ))
+    responses = []
+    for restaurant_id, url, day in urls:
+        try:
+            response = requests.get(
+                f"{url}?p_p_id=foodView_WAR_foodportlet"
+                f"&_foodView_WAR_foodportlet_sFoodDateYear={day.year}"
+                f"&_foodView_WAR_foodportlet_sFoodDateMonth={day.month - 1}"
+                f"&_foodView_WAR_foodportlet_sFoodDateDay={day.day}",
+            )
+            responses.append((restaurant_id, response, day))
+        except ChunkedEncodingError:
+            pass
+    job_list = [
+        get_menu_data(session, restaurant_id, response, day)
+        for restaurant_id, response, day in responses
+    ]
     await asyncio.gather(*job_list)
     session.close()
 
--- src/scripts/menu.py
+++ src/scripts/menu.py
@@ -8,7 +8,12 @@ from sqlalchemy.orm import Session
 from models import Menu
 
 
-async def get_menu_data(db_session: Session, restaurant_id: int, response: Response, day: datetime.datetime) -> None:
+async def get_menu_data(
+        db_session: Session,
+        restaurant_id: int,
+        response: Response,
+        day: datetime.datetime
+) -> None:
     menu_items: list[dict] = []
     soup = BeautifulSoup(response.text, "html.parser")
     for inbox in soup.find_all("div", {"class": "in-box"}):
@@ -17,13 +22,15 @@ async def get_menu_data(db_session: Session, restaurant_id: int, response: Respo
             if list_item.find("h3"):
                 menu = list_item.find("h3").text.replace("\t", "").replace("\r\n", "")
                 p = list_item.find("p", {"class": "price"}).text
-                menu_items.append(dict(
+                menu_item = dict(
                     restaurant_id=restaurant_id,
                     feed_date=day.strftime("%Y-%m-%d"),
                     time_type=title,
                     menu_food=str(menu).strip(),
                     menu_price=p,
-                ))
+                )
+                if menu_item not in menu_items:
+                    menu_items.append(menu_item)
     db_session.execute(delete(Menu).where(and_(
         Menu.restaurant_id == restaurant_id,
         Menu.feed_date == day.strftime("%Y-%m-%d"),
