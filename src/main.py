5539121dc49fc24d562d31d92ee5e9556af173c1
Jeongin Lee <jil8885@hanyang.ac.kr>
2024-03-31 19:37:31 +0900

    chore: Upgrade dependencies to 3.12

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import asyncio
+import ssl
 from datetime import datetime, timedelta
 
 import requests
+import urllib3
+from requests.adapters import HTTPAdapter
 from requests.exceptions import ChunkedEncodingError
 from sqlalchemy import select
 from sqlalchemy.exc import OperationalError
@@ -12,6 +15,19 @@ from scripts.menu import get_menu_data
 from utils.database import get_db_engine
 
 from utils.database import get_master_db_engine
+
+
+class HTTPSAdapter(HTTPAdapter):
+    def init_poolmanager(self, connections, maxsize, block=False, **kwargs):
+        ctx = ssl.create_default_context()
+        ctx.set_ciphers("AES256-GCM-SHA384")
+        self.poolmanager = urllib3.PoolManager(
+            num_pools=connections,
+            maxsize=maxsize,
+            block=block,
+            ssl_version=ssl.PROTOCOL_TLSv1_2,
+            ssl_context=ctx,
+        )
 
 
 async def main():
@@ -44,17 +60,19 @@ async def execute_script(session):
                 day,
             ))
     responses = []
-    for restaurant_id, url, day in urls:
-        try:
-            response = requests.get(
-                f"{url}?p_p_id=foodView_WAR_foodportlet"
-                f"&_foodView_WAR_foodportlet_sFoodDateYear={day.year}"
-                f"&_foodView_WAR_foodportlet_sFoodDateMonth={day.month - 1}"
-                f"&_foodView_WAR_foodportlet_sFoodDateDay={day.day}",
-            )
-            responses.append((restaurant_id, response, day))
-        except ChunkedEncodingError:
-            pass
+    with requests.Session() as request_session:
+        request_session.mount("https://", HTTPSAdapter())
+        for restaurant_id, url, day in urls:
+            try:
+                response = request_session.get(
+                    f"{url}?p_p_id=foodView_WAR_foodportlet"
+                    f"&_foodView_WAR_foodportlet_sFoodDateYear={day.year}"
+                    f"&_foodView_WAR_foodportlet_sFoodDateMonth={day.month - 1}"
+                    f"&_foodView_WAR_foodportlet_sFoodDateDay={day.day}",
+                )
+                responses.append((restaurant_id, response, day))
+            except ChunkedEncodingError:
+                pass
     job_list = [
         get_menu_data(session, restaurant_id, response, day)
         for restaurant_id, response, day in responses
