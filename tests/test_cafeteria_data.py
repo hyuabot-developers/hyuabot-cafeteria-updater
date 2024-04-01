fbc3b7eaaf227ea6a0dd664323be2b463b573a13
Jeongin Lee <jil8885@hanyang.ac.kr>
2024-04-01 10:13:16 +0900

    fix: Use HTTP adapter for unit testing

--- tests/test_cafeteria_data.py
+++ tests/test_cafeteria_data.py
@@ -8,6 +8,7 @@ from sqlalchemy import select
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session, sessionmaker
 
+from main import HTTPSAdapter
 from models import BaseModel
 from models import Restaurant, Menu
 from scripts.menu import get_menu_data
@@ -48,11 +49,13 @@ class TestFetchRealtimeData:
             for day_delta in range(-5, 5):
                 day = now + timedelta(days=day_delta)
                 urls.append((restaurant_id, f"https://www.hanyang.ac.kr/web/www/re{restaurant_id}", day))
-        responses = [(restaurant_id, requests.get(
-            f"{url}?p_p_id=foodView_WAR_foodportlet&_foodView_WAR_foodportlet_sFoodDateYear={day.year}"
-            f"&_foodView_WAR_foodportlet_sFoodDateMonth={day.month - 1}"
-            f"&_foodView_WAR_foodportlet_sFoodDateDay={day.day}",
-        ), day) for restaurant_id, url, day in urls]
+        with requests.Session() as request_session:
+            request_session.mount("https://", HTTPSAdapter())
+            responses = [(restaurant_id, request_session.get(
+                f"{url}?p_p_id=foodView_WAR_foodportlet&_foodView_WAR_foodportlet_sFoodDateYear={day.year}"
+                f"&_foodView_WAR_foodportlet_sFoodDateMonth={day.month - 1}"
+                f"&_foodView_WAR_foodportlet_sFoodDateDay={day.day}",
+            ), day) for restaurant_id, url, day in urls]
         job_list = [get_menu_data(session, restaurant_id, response, day) for restaurant_id, response, day in responses]
         await asyncio.gather(*job_list)
 
