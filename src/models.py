ed8c8636a302717835e3f4459da19d9d6a5edc1b
Jeongin Lee <jil8885@hanyang.ac.kr>
2026-04-27 10:08:01 +0900

    Revert "feat: Implement weather crawler"

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import ForeignKey, String, Double, PrimaryKeyConstraint, Integer, DateTime
+from sqlalchemy import ForeignKey, String, Double, PrimaryKeyConstraint
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 
@@ -33,20 +33,3 @@ class Menu(BaseModel):
     time_type: Mapped[str] = mapped_column(String(10), nullable=False)
     menu_food: Mapped[str] = mapped_column(String(400), nullable=False)
     menu_price: Mapped[str] = mapped_column(String(30), nullable=False)
-
-
-class NoticeCategory(BaseModel):
-    __tablename__ = 'notice_category'
-    category_id: Mapped[int] = mapped_column(Integer, nullable=False, primary_key=True)
-    category_name: Mapped[str] = mapped_column(String(20), nullable=False)
-
-
-class Notice(BaseModel):
-    __tablename__ = 'notices'
-    notice_id: Mapped[int] = mapped_column(Integer, nullable=False, primary_key=True, autoincrement=True)
-    title: Mapped[str] = mapped_column(String(100), nullable=False)
-    url: Mapped[str] = mapped_column(String(200), nullable=False)
-    expired_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), nullable=True)
-    category_id: Mapped[int] = mapped_column(ForeignKey('notice_category.category_id'), nullable=False)
-    user_id: Mapped[str] = mapped_column(String(20), nullable=False)
-    language: Mapped[str] = mapped_column(String(10), nullable=False)
